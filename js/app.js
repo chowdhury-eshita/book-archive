34ed4531be84aa75b84e3815c66c752b0b47b55f
chowdhury-eshita <chowdhuryeshita58@gmail.com>
2021-09-02 21:02:19 +0600

    update

--- js/app.js
+++ js/app.js
@@ -15,10 +15,12 @@ const searchBook = () => {
 }
 //search result display
 const displayBook = (data) => {
-    resultCountDisplay(data);
+    //result count function call
+    foundResultDisplay(data);
     const books = data.docs;
     // console.log(books);
     const searchResult = document.getElementById('search-result');
+    //clear display result area
     searchResult.textContent = '';
     books?.forEach(book => {
         const div = document.createElement('div');
@@ -26,40 +28,39 @@ const displayBook = (data) => {
         div.innerHTML = `
                 <div class="card h-100">
                 <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
-           <div class="card-body">
-                <h5 class="card-title">${book.title}</h5>
-               <p class="card-text">Author: ${book.author_name ? book.author_name : 'Not Available'}</p>
-               <p class="card-text">Publisher: ${book.publisher[0] ? book.publisher[0] : 'Not Available'}</p>
-               <p class="card-text">First Publish: ${book.first_publish_year}</p>
-
+                    <div class="card-body">
+                            <h5 class="card-title">${book.title}</h5>
+                        <p class="card-text">Author: ${book.author_name ? book.author_name : 'Not Available'}</p>
+                        <p class="card-text">Publisher: ${book.publisher[0] ? book.publisher[0] : 'Not Available'}</p>
+                        <p class="card-text">First Publish: ${book.first_publish_year}</p>
+                    </div>
+               </div>
                 `;
         searchResult.appendChild(div);
     });
+
 }
 
-//total result found
-const resultCountDisplay = (count) => {
-    const resultCount = document.getElementById('result-count');
+//result found section
+const foundResultDisplay = (count) => {
+    const foundResult = document.getElementById('found-result');
     if (!count.num_found) {
-        resultCount.innerHTML = `
+        foundResult.innerHTML = `
     <h5 class="text-secondary">${count.num_found} Result Found!</h5>
     `;
 
     }
     else {
-        resultCount.innerHTML = `
+        foundResult.innerHTML = `
         <h4 class="text-success fw-bold">${count.num_found} Results found</h4>
         `;
     }
 }
 
-const toggleResultCount = displayStyle => {
-    document.getElementById('result-count').style.display = displayStyle;
-}
 //empty searchbox
 const emptySearchBox = () => {
-    const errorMessage = document.getElementById('result-count');
+    const errorMessage = document.getElementById('found-result');
     errorMessage.innerHTML = `
-    <h5 class="text-danger">Please Enter Your Book Name!</h5>
+    <h5 class="text-danger">Please Enter a Book Name!</h5>
     `;
 }
